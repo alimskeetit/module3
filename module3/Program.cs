78d472d070a8784a7cae52484ffed79df78e7a1e
Ivan <afkanovich@yandex.ru>
2022-09-13 20:59:04 +0300

    3.3.2

--- module3/Program.cs
+++ module3/Program.cs
@@ -5,11 +5,15 @@ class MainClass
     public static void Main(string[] args)
     {
 
-        string name;
-        name = "Jane";
+        string MyName = "Vanya";
+        byte MyAge = 18;
+        bool HaveIApet = false;
+        double MyShoeSize = 43;
 
-        Console.WriteLine(name);
+        Console.WriteLine("My name is " + MyName);
+        Console.WriteLine("MyAge " + MyAge);
+        Console.WriteLine("Do I have a pet? " + HaveIApet);
+        Console.WriteLine("My shoe size is " + MyShoeSize);
 
-        Console.ReadKey();
     }
 }
