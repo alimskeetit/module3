8082f57a8fd0d5924fdb4b3484460c57f713be73
Ivan <afkanovich@yandex.ru>
2022-09-14 13:16:35 +0300

    switch case

--- module3/Program.cs
+++ module3/Program.cs
@@ -4,16 +4,38 @@ class MainClass
 {
     public static void Main(string[] args)
     {
+        Console.WriteLine("Write your favourite colour: ");
+        var color = Console.ReadLine();
 
-        string MyName = "Vanya";
-        byte MyAge = 18;
-        bool HaveIApet = false;
-        double MyShoeSize = 43;
+        switch (color)
+        {
+            case "red":
+                Console.BackgroundColor = ConsoleColor.Red;
+                Console.ForegroundColor = ConsoleColor.Black;
+                break;
 
-        Console.WriteLine("My name is " + MyName);
-        Console.WriteLine("MyAge " + MyAge);
-        Console.WriteLine("Do I have a pet? " + HaveIApet);
-        Console.WriteLine("My shoe size is " + MyShoeSize);
+            case "green":
+                Console.BackgroundColor = ConsoleColor.Green;
+                Console.ForegroundColor = ConsoleColor.Black;
+                break;
+            
+            default:
+                Console.BackgroundColor = ConsoleColor.Cyan;
+                Console.ForegroundColor = ConsoleColor.Black;
+                break;
+        }
+        Console.WriteLine("Text");
+        Console.ReadKey();
+    }
 
+    enum DayOfWeek
+    {
+        Monday = 1,
+        Tuesday,
+        Wednesday,
+        Thursday,
+        Friday,
+        Saturday,
+        Sunday
     }
 }
